fb2d676a01a907963d2ad21521d7cf0c14a04e27
Thomas Lin Pedersen <thomasp85@gmail.com>
2025-03-26 09:09:09 +0100

    fix bug in shared secret route

--- R/header_router.R
+++ R/header_router.R
@@ -4,11 +4,11 @@ create_header_router <- function(max_size, shared_secret) {
   if (!is.null(max_size)) {
     stack$add_route(routr::sizelimit_route(max_size), "max_size")
   }
-  if (!is.null(shared_secret)) {
+  if (!is.null(shared_secret) && shared_secret != "") {
     stack$add_route(routr::shared_secret_route(
       shared_secret,
       "Plumber-Shared-Secret"
-    ))
+    ), "shared_secret")
   }
   stack
 }
